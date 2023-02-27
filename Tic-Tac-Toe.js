115337fc3ad163927302897b4cfe094590d3f12b
Marco <Marco@example.com>
2023-02-27 13:06:35 +0100

    modifica funzione per renderizzare tabellone

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,30 +1,63 @@
+// Definiamo il nostro modulo
 const game = (() => {
+  // Funzione che rappresenta la nostra griglia da gioco
   const gameBoard = () => {
     let board = [
-      ["X", "", ""],
+      ["X", "O", ""],
       ["", "", ""],
-      ["", "", "O"],
+      ["", "", ""],
     ];
     return board;
   };
-  const Player = (name, icon) => {
-    const getName = () => name;
-    const getIcon = () => icon;
-  };
-  const displayController = () => {};
+  // Funzione fabbrica che crea oggetti persona
 
-  const gameBoardArray = () => {
-    let board = gameBoard();
-    let cellElements = document.querySelectorAll(".cell-grid");
-    let i = 0;
-    board.forEach((row) => {
-      row.forEach((cell) => {
-        cellElements[i].innerHTML = cell;
-        i++;
-      });
-    });
+  const Players = (name, icon) => {
+    const getPlayer = () => name;
+    const getIcon = () => icon;
+    return { getPlayer, getIcon };
   };
-  return { gameBoard, Player, displayController, gameBoardArray };
+
+  // Funzione che controlla il flusso di gioco
+
+  const displayController = () => {
+    let cellGrid = document.querySelectorAll(".cell-grid");
+    let i = 0;
+
+    // Funzione che renderà il contenuto dell'array
+    //  del tabellone di gioco sulla pagina web
+
+    const gameBoardArray = () => {
+      let board = gameBoard();
+      i = 0;
+      board.forEach((row) => {
+        row.forEach((cell) => {
+          cellGrid[i].innerHTML = cell;
+          i++;
+        });
+      });
+    };
+
+    // Funzione che consente di aggiungere il segno ad una cella
+    const getSign = (player) => {
+      cellGrid.forEach((cell) => {
+        cell.addEventListener(
+          "click",
+          () => (cell.innerHTML = player.getIcon())
+        );
+      });
+    };
+    return { gameBoardArray, getSign };
+  };
+
+  return { Players, displayController, gameBoard };
 })();
 
-game.gameBoardArray();
+// Creiamo un array di oggetti giocatore
+
+const player1 = game.Players("Player 1", "X");
+const player2 = game.Players("Player 2", "O");
+const players = [player1, player2];
+
+// utilizziamo la funzione displayController per renderizzare il tabellone di gioco
+const controller = game.displayController();
+controller.gameBoardArray();
